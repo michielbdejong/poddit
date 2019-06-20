7b2599b6fa9697b074f64094e50a8c0259ce2b44
Vincent <git@vincenttunru.com>
2019-06-21 01:45:35 +0200

    Try to login to a Pod if its passed as query param

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 import { LoggedIn, LogoutButton, LoggedOut, LoginButton } from '@solid/react';
+import auth from 'solid-auth-client';
 import { LinkSaver } from './components/LinkSaver';
 
 class App extends Component {
+  async login (identityProvider: string | null) {
+    if (!identityProvider) {
+      return;
+    }
+
+    const session = await auth.currentSession();
+    if (!session) {
+      auth.login(identityProvider);
+    }
+  }
+
   render() {
+    if (URLSearchParams && document.location.search.length > 0) {
+      const params = new URLSearchParams(document.location.search.substring(1));
+      if (params.has('idp')) {
+        this.login(params.get('idp'));
+        return (
+          <div>Logging you in, please stand by&hellip;</div>
+        );
+      }
+    }
+
     return (
       <div>
         
