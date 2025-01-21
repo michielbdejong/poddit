ac567a47346f0fa6b17dd95e68706b9327e2c983
Michiel de Jong <michiel@unhosted.org>
2025-01-21 09:28:58 +0000

    built on gitpod using node-10

---
+++ public/precache-manifest.157166ea8bd1747f72bd76a4148277d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "b1b7faa61b747d696aa7",
+    "url": "./static/js/main.831f3296.chunk.js"
+  },
+  {
+    "revision": "769f156ad86417b80b19",
+    "url": "./static/js/2.ba8a47d5.chunk.js"
+  },
+  {
+    "revision": "b1b7faa61b747d696aa7",
+    "url": "./static/css/main.b27d0a88.chunk.css"
+  },
+  {
+    "revision": "3e76156064824ba71b309a144f56ea5b",
+    "url": "./index.html"
+  }
+];
