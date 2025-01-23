b383c801ffdcb36c3cfd3e6e42637038d8098a27
Michiel de Jong <michiel@pondersource.com>
2025-01-23 17:07:22 +0100

    4 bookmarks in store reported - continue debugging from here tomorrow

--- app/store/getBookmarks.ts
+++ app/store/getBookmarks.ts
@@ -5,5 +5,6 @@ import { RDF, BOOKMARK } from '../../lib/namespaces';
 export async function getBookmarks(store: $rdf.IndexedFormula, webId: string): Promise<$rdf.Node[]> {
   await initialise(store, webId);
   const bookmarks = store.each(undefined, RDF('type'), BOOKMARK('Bookmark'), undefined);
+  console.log('in store', bookmarks);
   return bookmarks;
 }
